6c98450b703d3f1722404b62d0ad10cf632a4893
13zone <46686115+undergroundrp@users.noreply.github.com>
2019-09-28 18:07:02 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-bot_token = NjI3NTkyNTc0MDY4MzI2NDcx.XY-8RQ.9p43GEvKvpN72BZJfoM8RCHy5uA #token
+bot_token = <NjI3NTkyNTc0MDY4MzI2NDcx.XY_ndA.p9BRw3OYFDo-Qgeo9wb7qCluSRU> #token
 version = 'Beta' #version del bot
 message_limit=500
